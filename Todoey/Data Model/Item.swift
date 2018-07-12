afae50fa2b55edfa12347ee302b8294a8e2c0f96
agmoradi <m@ziholdings.com>
2018-07-12 01:35:23 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Co Co on 7/12/18.
+//  Copyright © 2018 Alvin Moradi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
